59ce0f3ae5f0ec3181b13b63029bb67171266809
sobershxts <avrane.22@ichat.sp.edu.sg>
2023-04-27 12:41:58 +0800

    Lab2 final

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,39 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+import statistics
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+
+def get_user_input():
+    txt = input("User Input: ")
+    x = txt.split(",")
+    print("User input: " + str(x))
+    numbers = [float(n) for n in x]
+    return numbers
+
+
+def calc_average_temperature(numbers):
+    avg = sum(numbers) / len(numbers)
+    print("Average temperature: " + str(avg))
+
+def calc_min_max_temperature(numbers):
+    min_temp = min(numbers)
+    max_temp = max(numbers)
+    print("Lowest temperature: " + str(min_temp))
+    print("Highest temperature: " + str(max_temp))
+    numbers.sort(reverse=False)
+    print(numbers)
+
+def calc_median_temperature(numbers):
+    median = statistics.median(numbers)
+    print("The median temperature is " + str(median))
+
+
+
+
+
+
+display_main_menu()
+numbers = get_user_input()
+calc_average_temperature(numbers)
+calc_min_max_temperature(numbers)
+calc_median_temperature(numbers)
