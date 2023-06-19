a3de701e576443c5b409870bb9487a9391629a8d
sobershxts <67905472+sobershxts@users.noreply.github.com>
2023-06-20 07:02:42 +0800

    lab2 prac done

--- Lab2.py
+++ Lab2.py
@@ -20,12 +20,28 @@ def calc_min_max_temperature(numbers):
     max_temp = max(numbers)
     print("Lowest temperature: " + str(min_temp))
     print("Highest temperature: " + str(max_temp))
-    numbers.sort(reverse=False)
+    numbers.sort()
     print(numbers)
 
 def calc_median_temperature(numbers):
     median = statistics.median(numbers)
     print("The median temperature is " + str(median))
+
+def calc_complex_median_temp(numbers):
+    numbers.sort()
+    n = len(numbers)
+    if n % 2 == 0:
+        # the instance where there an even number of values, average the middle two
+        median = (numbers[n//2 - 1] + numbers[n//2]) / 2
+    else:
+        # if there are an odd number of values, take the median value
+        median = numbers[n//2]
+    print("The median temperature is " + str(median))
+
+#def calc_complex_median_temp(numbers):
+ #   numbers.sort()
+  #  median = statistics.median(numbers)
+   # print("The median temperature is " + str(median))
 
 
 
--- bmi.py
+++ bmi.py
@@ -5,9 +5,15 @@ def calculate_bmi(height, weight):
     print(bmi)
     if bmi < 18.5:
         print("Under Weight")
+        print(int(-1))
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("Normal Weight")
+        print(int(0))
+        return 0
     elif bmi > 25.0:
         print("Over Weight")
+        print(int(1))
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
